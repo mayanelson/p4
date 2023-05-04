b9bbcf0ce4217f23afa823d91150564680790fb7
Maya Nelson <mnelson30@stuy.edu>
2023-05-04 09:26:22 -0400

    did good

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+# YAMMERS
+# SoftDev
+
+from flask import Flask, render_template
+app = Flask(__name__)  
+
+@app.route("/")       
+def hello_world():
+    return render_template('main.html')
+
+if __name__ == "__main__":  # true if this file NOT imported
+    app.debug = True        # enable auto-reload upon code change
+    app.run()

63edf33bdd755730cf4609c80e7c0c1d050108c7
Maya Nelson <mnelson30@stuy.edu>
2023-05-12 09:21:56 -0400

    cool game

--- app/static/js/test.js
+++ app/static/js/test.js
@@ -3,21 +3,36 @@
 window.addEventListener("DOMContentLoaded", (event) => {
   var hButton = document.getElementById("higherButton"); 
   var lButton = document.getElementById("lowerButton"); 
-  var sample = 10000;
-  var sample2 = 15000;
+  var nextButton = document.getElementById("nextButton");
+  var sample = Math.floor(Math.random()*10000);
+  var sample2 = Math.floor(Math.random()*10000);
+
+  var str = sample + " is HIGHER or LOWER than " + sample2;
+
+  var clicked = false;
+
+  
+      
+  document.getElementById('text').innerHTML = str;
+  console.log("WORKED");
+  document.getElementById("nextButton").style.visibility="hidden";
 
   if (hButton == null) {
       console.log("hButton is null");
   }
   else {
   hButton.addEventListener("click", function() {
-      if (sample < sample2) {
+      if (sample > sample2) {
           document.getElementById("para").innerHTML = "DING DING DING";
+          document.getElementById("para").style.color = "green";
       }
       else {
           document.getElementById("para").innerHTML = "WRONG";
+          document.getElementById("para").style.color = "red";
       }
+      clicked = true;
       console.log("higher clicked");
+      document.getElementById("nextButton").style.visibility="visible";
     });
   }
   
@@ -26,14 +41,30 @@ window.addEventListener("DOMContentLoaded", (event) => {
   }
   else {
   lButton.addEventListener("click", function() {
-      if (sample < sample2) {
+      if (sample > sample2) {
           document.getElementById("para").innerHTML = "WRONG";
+          document.getElementById("para").style.color = "red";
       }
       else {
           document.getElementById("para").innerHTML = "DING DING DING";
+          document.getElementById("para").style.color = "green";
       }
-      console.log("higher clicked");
+      console.log("lower clicked");
+      clicked = true;
+      document.getElementById("nextButton").style.visibility="visible";
     });
   }
+
+  if (nextButton == null) {
+    console.log("nextButton is null");
+}
+else {
+nextButton.addEventListener("click", function() {
+    
+    console.log("next clicked");
+    clicked = false;
+    document.getElementById("nextButton").style.visibility="hidden";
+  });
+}
   });
   
